20645c15d2385b32a880ebc3c292239f3875bba7
kian-nosratian <kiannosratian76@gmail.com>
2025-01-01 01:45:22 +0330

    work on onloading page

--- filemanager/lib/pages/onloading/onloading.dart
+++ filemanager/lib/pages/onloading/onloading.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -9,9 +11,170 @@ class ONLOADING extends StatelessWidget{
 
   Widget build(BuildContext context) {
       return Scaffold(
+
         body: Center(
-          child: Text("ONLOADING"),
+          child:Container(
+            width:375,
+            height: 812, 
+            decoration: BoxDecoration( 
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomLeft , 
+                colors:[
+                  const Color.fromARGB(255, 97, 3, 248), 
+                  const Color.fromARGB(255, 50, 4, 136), 
+                ]
+              ),
+              border: Border.all(
+              width: 1 , 
+              color: Colors.red, 
+            ), 
+            borderRadius: BorderRadius.all(
+              Radius.circular(50),
+            ), 
+          ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:<Widget>[
+                      Container(
+                        width: 210,
+                        height: 200,
+                        decoration: BoxDecoration( 
+                          color:  Color.fromARGB(0, 0, 0, 0), 
+                        ),
+                        child: Stack(
+                          children:<Widget>[
+
+                            Positioned(
+                              top: 40,
+                              left: 40, 
+                              child: SizedBox(
+                               child: Image.asset(
+                                "assets/Document.png",
+                                fit: BoxFit.cover,
+                                height: 60, 
+                                width: 60,
+                              ),
+                           ), 
+                          ), 
+
+                            Positioned(
+                              top: 4,
+                              right: 30, 
+                              child: SizedBox(
+                               child: Image.asset(
+                                "assets/Music.png",
+                                fit: BoxFit.cover,
+                                height: 60, 
+                                width: 60,
+                              ),
+                           ), 
+                          ), 
+
+                            Positioned(
+                              top: 80,
+                              right: 30, 
+                              child: SizedBox(
+                               child: Image.asset(
+                                "assets/Picture.png",
+                                fit: BoxFit.cover,
+                                height: 60, 
+                                width: 60,
+                              ),
+                           ), 
+                          ), 
+                           
+                            Positioned(
+                              top: 120,
+                              left: 30, 
+                              child: SizedBox(
+                               child: Image.asset(
+                                "assets/Vector.png",
+                                fit: BoxFit.cover,
+                                height: 60, 
+                                width: 60,
+                              ),
+                           ), 
+                          ), 
+                           
+                           
+                           
+
+
+
+
+
+                          ],
+                        ),
+                      ),
+
+                      Container( 
+                        width: 209,
+                        height: 192,
+                        decoration: BoxDecoration( 
+                          color:  Color.fromARGB(0, 0, 0, 0),
+                        ),
+                        child: Image.asset(
+                                "assets/logo.png",
+                                fit: BoxFit.cover,
+                                height: 192, 
+                                width: 208,
+                              ),
+
+
+
+                      ),
+                      
+                      Container( 
+                        width: 315,
+                        height: 148,
+                        decoration: BoxDecoration( 
+                          border: Border.all(
+                            width: 1 , 
+                            color: Colors.red
+                          )
+                        ),
+                      ),
+              
+                      Container( 
+                        width: 122,
+                        height: 122,
+                        decoration: BoxDecoration( 
+                          border: Border.all(
+                            width: 1 , 
+                            color: Colors.red
+                          )
+                        ),
+                      )
+
+
+
+              ],
+            ),
+
+
+
+
+
         ),
-      );
+      )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
   }
 }
